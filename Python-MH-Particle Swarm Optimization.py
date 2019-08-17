48b221247cdcf56aeb9d7728ac2ecd006ae5eaf6
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-08-17 09:57:22 -0300

    Update Python-MH-Particle Swarm Optimization.py

--- Python-MH-Particle Swarm Optimization.py
+++ Python-MH-Particle Swarm Optimization.py
@@ -58,12 +58,11 @@ def velocity_vector(position, init_velocity, i_b_matrix, best_global, w = 0.5, c
 
 # Function: Updtade Position
 def update_position(position, velocity, min_values = [-5,-5], max_values = [5,5], target_function = target_function):
-    updated_position = np.copy(position)
     for i in range(0, position.shape[0]):
         for j in range(0, position.shape[1] - 1):
-            updated_position[i,j] = np.clip((position[i,j] + velocity[i,j]),  min_values[j],  max_values[j])
-        updated_position[i,-1] = target_function(updated_position[i,0:updated_position.shape[1]-1])
-    return updated_position
+            position[i,j] = np.clip((position[i,j] + velocity[i,j]),  min_values[j],  max_values[j])
+        position[i,-1] = target_function(position[i,0:position.shape[1]-1])
+    return position
 
 # PSO Function
 def particle_swarm_optimization(swarm_size = 3, min_values = [-5,-5], max_values = [5,5], iterations = 50, decay = 0, w = 0.9, c1 = 2, c2 = 2, target_function = target_function):    
